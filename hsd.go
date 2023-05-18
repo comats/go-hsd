ab89ad8cd94af6df1b638908f7223b83f5565200
澤田慶 <kei@sawadakeinoMacBook-Pro.local>
2023-05-18 10:03:43 +0900

    test file added.

--- hsd.go
+++ hsd.go
@@ -1,18 +1,25 @@
+// Package foo provide functions for example
 package hsd
 
+import "fmt"
+
+// Add returns a plus b.
 func StringDistance(lhs, rhs string) int {
 	return Distance([]rune(lhs), []rune(rhs))
 }
 
+// Sub returns a minus b.
 func Distance(a, b []rune) int {
 	dist := 0
 	if len(a) != len(b) {
 		return -1
 	}
 	for i := range a {
+		fmt.Printf("a[%d]: %v  b[%d]: %v\n", i, a[i], i, b[i])
 		if a[i] != b[i] {
 			dist++
 		}
 	}
+	fmt.Printf("distance is %d\n", dist)
 	return dist
 }
